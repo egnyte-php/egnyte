6b2dd85a42cdfbad19b94d4f3c92f54d96ce1825
Tom Stovall <119924+stovak@users.noreply.github.com>
2022-04-30 17:42:55 -0700

    Adding tests

--- tests/FileTest.php
+++ tests/FileTest.php
@@ -3,13 +3,12 @@
 namespace EgnytePhp\Egnyte\Tests;
 
 use EgnytePhp\Egnyte\Model\File;
-use GuzzleHttp\Client;
 use GuzzleHttp\Psr7\Response;
+use GuzzleHttp\Psr7\Uri;
+use League\OAuth2\Client\Provider\GenericProvider;
+use League\OAuth2\Client\Token\AccessToken;
 use PHPUnit\Framework\TestCase;
-use Sainsburys\Guzzle\Oauth2\AccessToken;
-use Sainsburys\Guzzle\Oauth2\GrantType\PasswordCredentials;
-use Sainsburys\Guzzle\Oauth2\GrantType\RefreshToken;
-use Sainsburys\Guzzle\Oauth2\Middleware\OAuthMiddleware;
+
 
 /**
  *
@@ -28,15 +27,16 @@ class FileTest extends TestCase {
    * @test
    */
   public function testBaseURI() {
-    $file = new File('betweenlines');
-    $this->assertEquals("betweenlines.egnyte.com/pubapi/v1", $file->getBaseUri());
+    $domain = getenv('EGNYTE_DOMAIN');
+    $file = new File($domain);
+    $this->assertEquals("https://{$domain}.egnyte.com/pubapi/v1", $file->getBaseUri());
   }
 
   /**
    * @test
    */
   public function testOauthHeader() {
-    $file = new File('betweenlines', '12345678901234567890');
+    $file = new File('something', '12345678901234567890');
     $config = $file->getClient()->getConfig();
     $this->assertIsArray($config['headers'], "client config should have headers");
     $this->assertArrayHasKey("Authorization", $config['headers'], "headers config should show authorization header");
@@ -46,41 +46,42 @@ class FileTest extends TestCase {
   /**
    * @test
    */
-  public function testGetDirectory() {
+  public function testBasicFileFunctions() {
+    $domain = getenv('EGNYTE_DOMAIN');
     $token = $this->getAccessToken();
     $this->assertIsString($token, "Token should be returned as string");
-    $file = new File('betweenthelinestranslations', );
+    $file = new File($domain, $token);
 
-    $dirlist = $file->listFolder("/Shared Files");
+    $dirlist = $file->listFolder("/Shared");
     $this->assertInstanceOf(Response::class, $dirlist,);
     $this->assertTrue(in_array($dirlist->getStatusCode(), [
       200,
       201,
       202,
       203,
-    ]), "Status code should be one of the 200's");
+    ]), "Status code should be one of the 200's: " . $dirlist->getStatusCode());
     $this->assertNotEmpty($dirlist->getBody(), "Returned list should not be empty.");
 
   }
 
   public function getAccessToken(): string {
-    $urls = parse_url(getenv('EGNYTE_TOKEN_URL'));
+    $url = new Uri(getenv('EGNYTE_TOKEN_URL'));
     $config = json_decode(getenv("EGNYTE_KEY_VALUE"), TRUE);
-    $config['token_url'] = $urls['path'];
-    $oauthClient = new Client([
-      'base_uri' => $urls['host'],
-      'debug' => TRUE,
-      'verify' => FALSE,
-      'http_errors' => FALSE,
+    $baseUri = str_replace($url->getPath(), "", (string) $url);
+    $provider = new GenericProvider([
+      'clientId'                => $config['clientId'],    // The client ID assigned to you by the provider
+      'clientSecret'            => $config['clientSecret'],    // The client password assigned to you by the provider
+      'urlAccessToken'          => (string) $url,
+      'urlResourceOwnerDetails' => $baseUri . '/pubapi/v1/userinfo',
+      'urlAuthorize'            => $baseUri . '/pubapi/v1/authorize'
     ]);
-    $middleware = new OAuthMiddleware(
-      $oauthClient,
-      new PasswordCredentials($oauthClient, $config),
-      new RefreshToken($oauthClient, $config)
-    );
-    $tokenObject = $middleware->getAccessToken();
-    $this->assertInstanceOf(AccessToken::class, $tokenObject, "Should be isntance of Access Token");
-    return $tokenObject->getToken();
+    $token = $provider->getAccessToken('password', [
+      'username' => $config['username'],
+      'password' => $config['password']
+    ]);
+    $this->assertInstanceOf(AccessToken::class, $token, "Should be instance of Access Token");
+    $this->assertIsString($token->getToken(), "Should be access Token String");
+    return $token->getToken();
   }
 
 }
